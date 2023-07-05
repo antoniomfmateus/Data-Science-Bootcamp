db0e977be03786240fa0623eab3c4846b391aaf8
antonio mateus <antoniomateus.dev@gmail.com>
2023-07-05 15:53:59 +0100

    Python

---
+++ 01_Python/01_Python_for_Data_Science/01-Sum-Of-Three/sum_of_three.py
@@ -0,0 +1,3 @@
+def sum3(first_number, second_number, third_number):
+        return first_number + second_number + third_number
+    
---
+++ 01_Python/01_Python_for_Data_Science/06-Environment/flask_option.py
@@ -0,0 +1,11 @@
+import os
+
+def start():
+    """returns the right message"""
+    env = os.getenv(key = 'FLASK_ENV', default="empty")
+
+    return f"Starting in {env} mode..."
+
+
+if __name__ == "__main__":
+    print(start())
